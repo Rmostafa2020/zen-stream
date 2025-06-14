dfe5d02b15d321d65473b9f6f4a81e971d0b8b1c
tanzilarahman <trahman8569@gmail.com>
2025-06-13 23:29:09 -0400

    signin page layout, setup

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from "next/image";
+import "./homepage.css"
 import { SignInButton, SignUpButton } from "@clerk/nextjs"; // Import Clerk buttons
 
 
@@ -7,19 +8,17 @@ export default function Home() {
   return (
     <div className="home-container">
       <header className="home-header">
-        <h1>Welcome to My Application</h1>
+        <h1>
+        <p className="home-greeting">Hello</p>
+        <p className="home-greeting"> Welcome to ZenStream!</p> 
+        <p className="home-greeting">Stream Serenity, Powered by AI.</p>
+    </h1>
       </header>
       <main className="home-main">
-        <Image
-          src="/images/Screenshot.png" // Replace with your image path
-          alt="Screenshot"
-          width={600}
-          height={400}
-          className="home-image"
-        />
-        <p className="home-description">
+        
+        {/* <p className="home-description">
           This is a sample page created using Next.js and Clerk for authentication.
-        </p>
+        </p> */}
         <div className="auth-buttons">
           <SignInButton>
             <button className="auth-button">Sign In</button>
