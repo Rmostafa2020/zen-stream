6ce9ad871b8418cc18a1dcd7f850e2f28555a371
farhikhta farzan <farhikhtafarzan@gmail.com>
2025-06-16 15:55:10 -0400

    delete user updated

--- pages/api/test.js
+++
@@ -1,8 +0,0 @@
-// pages/api/test.js
-import dbConnect from "@/backend/models/lib/mongodb";
-
-export default async function handler(req, res) {
-  await dbConnect();
-
-  res.status(200).json({ message: "Connected to MongoDB Atlas from Next.js!" });
-}
